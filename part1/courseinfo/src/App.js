7684d0ef2f270bcd1f19572acdcd7970ba8ca94e
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-06 12:10:34 +1000

    1.5 further refactoring

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -22,32 +22,37 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  const parts = props.parts
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
 }
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
