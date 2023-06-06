7f7ba437a239a297d3b62c29be5ac791b485971d
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-07 09:24:07 +1000

    2.8 add number field

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '000-3232-3232' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleAddPerson = (event) => {
     event.preventDefault()
-    const newPerson = {name: newName}
+    const newPerson = {name: newName, number: newNumber}
     if(persons.filter(person => JSON.stringify(person) === JSON.stringify(newPerson)).length > 0){
       alert(`${newName} is already added to phonebook`)
     } else {
       setPersons([...persons, newPerson])
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -25,11 +27,14 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type="submit" onClick={handleAddPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
