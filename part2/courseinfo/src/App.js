416b53af4b119e1ba75d0cbccc34d0d6b2133d4f
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-06 18:14:23 +1000

    2.1 add course component and fix to work for any number of parts

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -10,24 +10,26 @@ const Part = (props) => {
   )
 }
 
-const Content = (props) => {
-  const parts = props.parts
+const Content = ({parts}) => {
   return (
-    <>
-      <Part part={parts[0].name} exercises={parts[0].exercises}/>
-      <Part part={parts[1].name} exercises={parts[1].exercises}/>
-      <Part part={parts[2].name} exercises={parts[2].exercises}/>
-    </>
+      parts.map(p => <Part part={p.name} exercises={p.exercises} key={p.name}/>)
   )
 }
 
-const Total = (props) => {
-  const parts = props.parts
+const Total = ({parts}) => {
   return (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    <p>Number of exercises {parts.reduce((n, {exercises}) => n + exercises, 0)}</p>
   )
 }
-
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
 
 const App = () => {
   const course = {
@@ -48,13 +50,7 @@ const App = () => {
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
