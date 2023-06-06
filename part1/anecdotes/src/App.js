9c15a368cfe4b9d8becb7cad3024d5b3ce32958e
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-06 13:08:10 +1000

    1.13 add voting

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,10 +13,16 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0)  )
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
+      <p>{anecdotes[selected]} has {votes[selected]} votes</p>
+      <button onClick={() => {
+          const copy = [...votes]
+          copy[selected] += 1
+          setVotes(copy)
+      }}>vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
     </div>
   )
