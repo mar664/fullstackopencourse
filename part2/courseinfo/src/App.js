53662638942d3bbb8089385c7145051cbb4631cd
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-07 08:56:34 +1000

    2.5 add Course as separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,35 +1,4 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-  return (
-      <p>
-        {props.part} {props.exercises}
-      </p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-      parts.map(p => <Part part={p.name} exercises={p.exercises} key={p.name}/>)
-  )
-}
-
-const Total = ({parts}) => {
-  return (
-    <b>total of {parts.reduce((n, {exercises}) => n + exercises, 0)} exercises</b>
-  )
-}
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from "./Course"
 
 const App = () => {
   const courses = [
