d0e2917560fa76a0475d94fba2410829d57c88ff
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-09 09:31:13 +1000

    continue optimising structure

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,22 +1,32 @@
-require('dotenv').config()
-
+const config = require('./utils/config')
 const express = require('express')
-
 const app = express()
-
 const blogsRouter = require('./controllers/blogs')
-
 const cors = require('cors')
-
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const mongoUrl = process.env.MONGODB_URI
+mongoose.set('strictQuery', false)
 
-mongoose.connect(mongoUrl)
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
