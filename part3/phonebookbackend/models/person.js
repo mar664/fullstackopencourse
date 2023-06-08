ff36f66c25cf795bffb39791a1dbaa608382924c
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-08 16:25:59 +1000

    3.20 add number validator

--- part3/phonebookbackend/models/person.js
+++ part3/phonebookbackend/models/person.js
@@ -21,7 +21,15 @@ const personSchema = new mongoose.Schema({
         required: true
     },
     number: {
-        type: String
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{5,}$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        minLength: 8,
+        required: true
     }
 })
 
