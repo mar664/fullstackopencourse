2ed6c5f65afa63234ad3d57c3867edcad8140857
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-08 10:30:57 +1000

    3.2 add info endpoint

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -29,6 +29,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const content = `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  response.send(content)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
