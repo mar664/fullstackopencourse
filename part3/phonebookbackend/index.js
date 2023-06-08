cc2926356f8f86bea3ed7f161d0103050e9cb0c0
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-08 13:25:15 +1000

    3.10 deploy phonebook backend

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -78,7 +78,8 @@ app.get('/info', (request, response) => {
   response.send(content)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
