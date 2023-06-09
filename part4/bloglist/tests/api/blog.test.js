c4a0ee89da0d37ce26210a3c35711c79ec3650ff
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-09 16:12:04 +1000

    move blog tests into block

--- part4/bloglist/tests/api/blog.test.js
+++ part4/bloglist/tests/api/blog.test.js
@@ -22,16 +22,17 @@ afterAll(async () => {
   await close_connection()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('blog testing', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
 })
-
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(initialBlogs.length)
-})
-
