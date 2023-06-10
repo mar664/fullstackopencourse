01b2b7bed2cf4d088360da36315b3beb80adab2a
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-10 14:48:56 +1000

    Add token based auth

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,12 +5,14 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const cors = require('cors')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
