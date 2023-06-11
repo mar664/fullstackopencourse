2e9d5f65ba7a3d0abc13b2abcc5f0cf8a217ade5
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-11 13:52:47 +1000

    5.14 test that url and likes shown when show is clicked

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -42,4 +43,14 @@ describe('<Blog />', () => {
     const blogAdditionalContainer = container.querySelector('.blog-additional-content')
     expect(blogAdditionalContainer).toHaveStyle('display: none')
     })
+
+    test('after clicking the button, url and likes displayed', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('show')
+        await user.click(button)
+      
+        const blogAdditionalContainer = container.querySelector('.blog-additional-content')
+        expect(blogAdditionalContainer).not.toHaveStyle('display: none')
+      })
+      
 })
