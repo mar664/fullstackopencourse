c55d40b6e56516279488b151e4133a07482f2119
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-11 11:24:09 +1000

    fix login to change to blog page on successful login

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   const handleLogout = async () => {
     setUser(null)
     window.localStorage.removeItem(
-      'loggedBlogAppUser'
+      'loggedBlogappUser'
     )
   }
 
