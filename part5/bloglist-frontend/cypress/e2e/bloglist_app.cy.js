dfd7992d607a0165bc513bcf81922781a5184c4c
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-12 10:42:57 +1000

    5.19 add test for create new blog

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -8,6 +8,7 @@ describe('Bloglist app', function() {
       username: 'mluukkai',
       password: 'salainen'
     }
+
     cy.request('POST', `${Cypress.env('base_backend_url')}/users/`, user) 
 
     cy.visit(Cypress.env('base_frontend_url'))
@@ -38,6 +39,36 @@ describe('Bloglist app', function() {
 
       cy.contains('Wrong credentials')
       cy.get('#error-message').should('have.css', 'color', 'rgb(255, 0, 0)').and('have.css', 'border-color', 'rgb(255, 0, 0)')
+    })
+  })
+
+  describe('When logged in', function() {
+    const blog = {
+      title: 'My New Blog Title',
+      url: 'http://test.com/postUrl',
+      author: 'Myself'
+    }
+
+    beforeEach(function() {
+      cy.request('POST', `${Cypress.env('base_backend_url')}/login`, {
+        username: user.username, password: user.password
+      }).then(response => {
+        localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+        cy.visit(Cypress.env('base_frontend_url'))
+      })
+    })
+
+    it('A blog can be created', function() {
+      cy.get('button').contains('create blog').click()
+      cy.get('input[placeholder="enter title"]').type(blog.title)
+      cy.get('input[placeholder="enter url"]').type(blog.url)
+      cy.get('input[placeholder="enter author"]').type(blog.author)
+      cy.get('input[value="create"]').click()
+      cy.get('#success-message')
+      .should('have.css', 'color', 'rgb(0, 128, 0)')
+      .and('have.css', 'border-color', 'rgb(0, 128, 0)')
+      .and('contain.text', `a new blog ${blog.title} by ${blog.author} added`)
+      cy.get('.blog').contains(blog.title)
     })
   })
 })
