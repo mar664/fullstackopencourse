ebcc99b8e4708d738a7c941e68c5337867362014
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-14 13:09:13 +1000

    add ability in backend to store and receive comments

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -17,8 +17,8 @@ blogsRouter.post('/', async (request, response) => {
 
   user.blogs = user.blogs.concat(result._id);
   await user.save();
-
-  response.status(201).json(result.populate('user', { blogs: 0 }));
+  await result.populate('user', { blogs: 0 });
+  response.status(201).json(result);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
@@ -50,6 +50,24 @@ blogsRouter.put('/:id', async (request, response) => {
   }).populate('user', { blogs: 0 });
 
   response.json(updatedBlog);
+});
+
+blogsRouter.post('/comments/:id', async (request, response) => {
+  const { comment } = request.body;
+
+  const { id } = request.params;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    id,
+    { $push: { comments: comment } },
+    {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    }
+  ).populate('user', { blogs: 0 });
+
+  response.status(201).json(updatedBlog);
 });
 
 module.exports = blogsRouter;
