8241d06764adfade1945230cfe249795a85c2da9
Matthew Ruse <matthew.ruse@gmail.com>
2023-06-17 09:38:38 +1000

    8.17 implement login in frontend app

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -3,10 +3,14 @@ import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import { Routes, Route, Link } from "react-router-dom";
 import Notify from "./components/Notify";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import LoginForm from "./components/LoginForm";
+import { useNavigate } from "react-router-dom";
+
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
-
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
   const notify = (message) => {
     setErrorMessage(message);
     setTimeout(() => {
@@ -14,25 +18,49 @@ const App = () => {
     }, 10000);
   };
 
+  const logout = (e) => {
+    localStorage.clear();
+    setToken(null);
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("library-user-token");
+    setToken(token);
+  }, []);
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
       <div>
-        <Link to="/">
+        <Link to="/authors">
           <button>authors</button>
         </Link>
-        <Link to="/books">
+        <Link to="/">
           <button>books</button>
         </Link>
-        <Link to="/add">
-          <button>add book</button>
-        </Link>
+        {token ? (
+          <>
+            <Link to="/add">
+              <button>add book</button>
+            </Link>
+            <button onClick={logout}>logout</button>
+          </>
+        ) : (
+          <Link to="/login">
+            <button>login</button>
+          </Link>
+        )}
       </div>
       <Routes>
-        <Route path="/" element={<Authors setError={notify} />} />
-        <Route path="/books" element={<Books setError={notify} />} />
+        <Route path="/authors" element={<Authors setError={notify} />} />
+        <Route
+          path="/login"
+          element={<LoginForm setToken={setToken} setError={notify} />}
+        />
+        <Route path="/" element={<Books setError={notify} />} />
         <Route path="/add" element={<NewBook setError={notify} />} />
-      </Routes>
+      </Routes>{" "}
     </div>
   );
 };
