74e2b90ec32c627531c0e6e56cd039172d640bcb
Matthew Ruse <matthew.ruse@gmail.com>
2023-07-03 12:07:38 +1000

    9.5 WebBMI

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,3 +1,4 @@
+import { calculateBmi } from "./calculateBMI";
 import { isNotNumber } from "./utils";
 
 interface BMIValues {
@@ -19,24 +20,6 @@ const parseArguments = (args: string[]): BMIValues => {
     }
 }
 
-/* Calculates the BMI given height in cm and weight in kg and returns a message conveying the result */
-const calculateBmi = (height : number, weight : number) : string => {
-    if(height <= 0) throw new Error("Height must be greater than zero")
-    if(weight <= 0) throw new Error("Weight must be greater than zero")
-
-    height = height / 100
-    const BMI = weight / (height*height)
-
-    if (BMI < 16.0) return "Underweight (Severe thinness)"
-    else if (BMI > 16.0 && BMI < 17.0) return "Underweight (Moderate thinness)"
-    else if (BMI >= 17.0 && BMI < 18.5) return "Underweight (Mild thinness)"
-    else if (BMI >= 18.5 && BMI < 25.0) return "Normal (healthy weight)"
-    else if (BMI >= 25.0 && BMI < 30.0) return "Overweight (Pre-obese)"
-    else if (BMI >= 30.0 && BMI < 35.0) return "Obese (Class I)"
-    else if (BMI >= 35.0 && BMI < 40.0) return "Obese (Class II)"
-    else if (BMI >= 40.0) return "Obese (Class III)"
-    throw new Error("BMI range not found")
-}
 
 try {
     const { height, weight } = parseArguments(process.argv);
--- part9/index.ts
+++ part9/index.ts
@@ -1,9 +1,29 @@
 import express from 'express'
+import { Request, Response } from "express"
+import { calculateBmi } from './calculateBMI';
+import { isNotNumber } from './utils';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req: Request, res: Response) => {
+    const { weight, height } = req.query;
+
+    try {    
+        if(isNotNumber(weight) || isNotNumber(height)) throw new Error("Height and weight must be numbers")
+        const bmi : string = calculateBmi(Number(height), Number(weight))
+        return res.send({height: Number(height), weight: Number(weight), bmi});
+    } catch (error: unknown) {
+        let errorMessage = ''
+        if (error instanceof Error) {
+            errorMessage += error.message;
+        }
+        console.log(errorMessage);
+        return res.status(401).send({ error: errorMessage });
+    }
+  });
 
 const PORT = 3003;
 
