dd98a8258e4aafd1e042cee39b8cd0c864c3d04b
Matthew Ruse <matthew.ruse@gmail.com>
2023-07-03 14:02:17 +1000

    9.7 WebExercises

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,3 +1,4 @@
+import { calculateExercises } from "./calculateExercises";
 import { isNotNumber } from "./utils";
 
 interface ExerciseValues {
@@ -16,56 +17,6 @@ const parseArguments = (args: string[]): ExerciseValues => {
     } else {
         throw new Error('Provided values were not numbers!');
     }
-};
-
-type Rating = 1 | 2 | 3;
-
-interface ExerciseResult {
-  periodLength: number;
-  trainingDays: number;
-  target: number;
-  average: number;
-  targetReached: boolean;
-  rating: Rating;
-  ratingExplanation: string;
-}
-
-/* Calculates the average time of daily exercise hours and compares it to the target amount of daily
-    exercise hours and compares it to the target amount of daily hours and returns an object
-*/
-const calculateExercises = (hours: Array<number>, target: number): ExerciseResult => {
-  if(target <= 0) throw new Error("target daily hours should be greater than zero");
-
-  const periodLength = hours.length;
-
-  if(periodLength === 0) throw new Error("period length must be greater than zero");
-
-  const trainingDays = hours.filter(h => h > 0).length;
-  const totalHours = hours.reduce((acc, h) => acc + h, 0);
-  const average = totalHours / periodLength;
-  const targetReached = average >= target;
-  
-  let rating: Rating = 1;
-  let ratingExplanation = 'not too bad but could be better';
-  if (targetReached) {
-    rating = 2;
-    ratingExplanation = 'average';
-    
-    if (average >= target * 1.5) {
-        rating = 3;
-        ratingExplanation = 'above average';
-    }
-  }
-  
-  return {
-    periodLength,
-    trainingDays,
-    target,
-    average,
-    targetReached,
-    rating,
-    ratingExplanation,
-  };
 };
 
 try {
--- part9/index.ts
+++ part9/index.ts
@@ -1,7 +1,10 @@
 import express = require('express');
 import { calculateBmi } from './calculateBMI';
 import { isNotNumber } from './utils';
+import { calculateExercises } from './calculateExercises';
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -14,6 +17,24 @@ app.get('/bmi', (req, res) => {
         if(isNotNumber(weight) || isNotNumber(height)) throw new Error("Height and weight must be numbers");
         const bmi : string = calculateBmi(Number(height), Number(weight));
         return res.send({height: Number(height), weight: Number(weight), bmi});
+    } catch (error: unknown) {
+        let errorMessage = '';
+        if (error instanceof Error) {
+            errorMessage += error.message;
+        }
+        console.log(errorMessage);
+        return res.status(401).send({ error: errorMessage });
+    }
+  });
+
+  app.post('/exercises', (req, res) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { daily_exercises, target } = req.body;
+
+    try {    
+        if(!Array.isArray(daily_exercises) || isNaN(target as number)) throw new Error("input data incorrect");
+        const result = calculateExercises(daily_exercises as Array<number>, target as number);
+        return res.send(result);
     } catch (error: unknown) {
         let errorMessage = '';
         if (error instanceof Error) {
