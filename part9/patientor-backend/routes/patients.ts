b99d3360b1886011b4a8f02904b9f302a13fe4aa
Matthew Ruse <matthew.ruse@gmail.com>
2023-07-04 09:59:26 +1000

    9.12 set up POST at /api/patients and test connection with backend with fake data

--- part9/patientor-backend/routes/patients.ts
+++ part9/patientor-backend/routes/patients.ts
@@ -1,9 +1,24 @@
-import express from 'express';
-import patientService from '../services/patientService';
+import express from "express";
+import patientService from "../services/patientService";
+import { NonSensitivePatient } from "../types";
 const router = express.Router();
 
-router.get('/', (_req, res) => {
+router.get("/", (_req, res) => {
   res.send(patientService.getNonSentitivePatients());
+});
+
+router.post("/", (req, res) => {
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+  const { name, dateOfBirth, gender, ssn, occupation } = req.body;
+
+  const addedPatient: NonSensitivePatient = patientService.addNewPatient({
+    name,
+    dateOfBirth,
+    gender,
+    ssn,
+    occupation,
+  });
+  res.send(addedPatient);
 });
 
 export default router;
--- part9/patientor-backend/services/patientService.ts
+++ part9/patientor-backend/services/patientService.ts
@@ -1,13 +1,31 @@
-import patientData from '../data/patients';
-
-import { NonSensitivePatient, Patient } from '../types';
+import patientData from "../data/patients";
+import { v1 as uuid } from "uuid";
+import { NewPatient, NonSensitivePatient, Patient } from "../types";
 
 const patients: Patient[] = patientData;
 
 const getNonSentitivePatients = (): NonSensitivePatient[] => {
-  return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({id, name, dateOfBirth, gender, occupation}));
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+const addNewPatient = (_patient: NewPatient): NonSensitivePatient => {
+  const id: string = uuid();
+  return {
+    id,
+    name: "Matti Luukkainen",
+    dateOfBirth: "1971-04-09",
+    gender: "male",
+    occupation: "Digital evangelist",
+  };
 };
 
 export default {
   getNonSentitivePatients,
+  addNewPatient,
 };
