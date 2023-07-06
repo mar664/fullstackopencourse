188d67a25ec3903536dcc93d247a2604ea507038
Matthew Ruse <matthew.ruse@gmail.com>
2023-07-07 08:53:52 +1000

    10.2 setup eslint and prettier

---
+++ part10/rate-repository-app/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: ["react", "react-native"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  env: {
+    "react-native/react-native": true,
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "react/react-in-jsx-scope": "off",
+  },
+  parserOptions: {
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+  },
+};
