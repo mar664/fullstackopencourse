78d89aa916297b02d9496de6f517b8ae3966f5ba
Matthew Ruse <matthew.ruse@gmail.com>
2023-07-13 08:52:48 +1000

    ex 13.13 filter blogs by title

--- part13/blogapp/controllers/blogs.js
+++ part13/blogapp/controllers/blogs.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { Op } = require("sequelize");
 
 const { Blog, User } = require("../models");
 const {
@@ -17,12 +18,19 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
+  const where = {};
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`,
+    };
+  }
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
       attributes: ["name", "username", "id"],
     },
+    where,
   });
   res.json(blogs);
 });
