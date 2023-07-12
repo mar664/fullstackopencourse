4fb03901f69a6f6ea41284ae628c6a585c5c0855
Matthew Ruse <matthew.ruse@gmail.com>
2023-07-12 16:16:51 +1000

    ex 13.5 update structure of app

--- part13/blogapp/index.js
+++ part13/blogapp/index.js
@@ -1,70 +1,20 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
+
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
+
+const blogsRouter = require("./controllers/blogs");
+
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+app.use("/api/blogs", blogsRouter);
 
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.STRING(50),
-    },
-    url: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  }
-);
-Blog.sync();
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-app.get("/api/blogs", async (req, res) => {
-  const notes = await Blog.findAll();
-  res.json(notes);
-});
-
-app.post("/api/blogs", async (req, res) => {
-  console.log(req.body);
-  const blog = await Blog.create(req.body);
-  res.json(blog);
-});
-
-app.get("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    res.json(blog);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  await Blog.destroy({ where: { id: req.params.id } });
-  res.status(204).end();
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
